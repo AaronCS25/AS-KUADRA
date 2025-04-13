9d9fc2bf766964dca2dfcbf0ddc2e26e16d0e047
AaronDev25 <aaron.camacho.25.09@gmail.com>
2025-04-13 00:12:41 -0500

    Add Parking model and establish relationship with User

--- app/features/auth/models.py
+++ app/features/auth/models.py
@@ -1,6 +1,8 @@
 from typing import Optional, List
 from sqlmodel import Field, SQLModel, Relationship
 
+from app.features.parking.models import Parking
+
 class UserRole(SQLModel, table=True):
     user_id: Optional[int] = Field(default=None, foreign_key="users.id", primary_key=True)
     role_id: Optional[int] = Field(default=None, foreign_key="roles.id", primary_key=True)
@@ -13,7 +15,10 @@ class User(SQLModel, table=True):
     full_name: str = Field(unique=True, index=True, nullable=False)
     is_active: bool = Field(default=True)
     password: str = Field(nullable=False)
+    
     roles: List["Role"] = Relationship(back_populates="users", link_model=UserRole)
+    parkings: List["Parking"] = Relationship(back_populates="owner")
+
 
 
 class Role(SQLModel, table=True):
---
+++ app/features/parking/models.py
@@ -0,0 +1,19 @@
+from sqlmodel import Field, SQLModel, Relationship
+from typing import Optional, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from app.features.auth.models import User
+
+class Parking(SQLModel, table=True):
+    __tablename__ = "parkings"  # type: ignore
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    title: str = Field(index=True, nullable=False)
+    location: str = Field(nullable=False)
+    capacity: int = Field(nullable=False)
+    available_spots: int = Field(nullable=False)
+    is_active: bool = Field(default=True)
+    is_verified: bool = Field(default=False)
+
+    owner_id: int = Field(foreign_key="users.id", nullable=False)
+    owner: "User" = Relationship(back_populates="parkings")
