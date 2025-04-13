9cbbac1986ecae0863430a63dfd293876c2db64e
AaronDev25 <aaron.camacho.25.09@gmail.com>
2025-04-12 22:37:15 -0500

    Add user repository functions for creating and retrieving users

---
+++ app/features/auth/repositories.py
@@ -0,0 +1,16 @@
+from sqlmodel import select
+from app.core.database import SessionDep, db_session
+from app.features.auth.models import *
+
+def create_user(user: User) -> None:
+    """Create a user."""
+    session: SessionDep = db_session.get()
+    session.add(user)
+    session.commit()
+
+def get_user(email: str) -> User | None:
+    """Get a user by email."""
+    session: SessionDep = db_session.get()
+    statement = select(User).where(User.email == email)
+    result = session.exec(statement).first()
+    return result
